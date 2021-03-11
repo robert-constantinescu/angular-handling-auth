47ae9c1563d02cb2ba2d4a20411b501f65a05d50
Robert Constantinescu <robertsci@github.com>
2021-03-11 20:18:03 +0200

    'removed'

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,15 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule,
-    ReactiveFormsModule
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
