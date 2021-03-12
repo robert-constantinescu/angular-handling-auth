3a308de43c8b3f5a4a618e82c65c7908b8873dea
Robert Constantinescu <robertsci@github.com>
2021-03-12 07:17:48 +0200

    Creating a custom validator to check if the passwords are matching

---
+++ src/app/auth/validators/password-match.ts
@@ -0,0 +1,26 @@
+import {AbstractControl, ValidationErrors, Validator} from '@angular/forms';
+
+
+// implementing the Validator interface is an OPTIONAL step
+export class PasswordMatch implements Validator {
+
+  // here we can also expect a FormGroup or FormControl,
+  // but both of these classes extends the AbstractControl class
+  validate(control: AbstractControl): ValidationErrors | null {
+    // in our case, the control argument will be a FormGroup object
+    // the control.value will match the values from the FormControls named 'password, passwordConfirmation'
+    const { password, passwordConfirmation } = control.value;
+
+    if (password === passwordConfirmation) {
+      // if there is no error we return Null
+      return null;
+    } else {
+      // we just return an object that says what is wrong. Here can be anything, 'passwordMatch' was chosen by Robert
+      return {passwordsMatch: false};
+    }
+
+  }
+
+
+
+}
