d9375e33486e310d2770ea8efa5dbe87d7ffcb72
Robert Constantinescu <robertsci@github.com>
2021-03-19 19:23:08 +0200

    added a BehaviorSubject to keep track of the authentication state of the user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-handling-auth';
+export class AppComponent implements OnInit {
+
+  signedin = false;
+
+  constructor(private authService: AuthService) {  }
+
+  ngOnInit() {
+    this.authService.signedin$.subscribe((isSignedIn) => {
+      this.signedin = isSignedIn;
+    });
+  }
+
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, Subject} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  // a BehaviorSubject(defaultValue) will emit the default value to any late subscriber
+  // if the .next(anotherValue) method will be called on the BehaviorSubject, all current subscribers and future
+  // will receive the last value emitted by the next() method. In this case `anotherValue`
+  signedin$ = new BehaviorSubject(false);
 
   private baseUrl = 'https://api.angular-email.com';
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+  }
 
   usernameAvailable(usernameCheck: string) {
     return this.http
@@ -22,7 +30,21 @@ export class AuthService {
       .post<SignupResponse>(
         `${this.baseUrl}/auth/signup`,
         formValues
-        );
+      ).pipe(
+        // an error coming out of the http observable is going to skip the tap() operator
+        // which is what we want, because an error from http observable means that we are probably not signed in
+        tap(() => {
+          this.signedin$.next(true);
+        })
+      );
+  }
+
+  signin() {
+
+  }
+
+  signout() {
+
   }
 
 }
