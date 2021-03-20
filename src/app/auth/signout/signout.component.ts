bc32451fdd7985626a52948d84566913dced5acf
Robert Constantinescu <robertsci@github.com>
2021-03-20 11:17:07 +0200

    added signed out component and some file for future tests

---
+++ src/app/auth/signout/signout.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth.service';
+
+@Component({
+  selector: 'app-signout',
+  templateUrl: './signout.component.html',
+  styleUrls: ['./signout.component.css']
+})
+export class SignoutComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.signout().subscribe(() => {
+      // navigate the user back to a signin page
+    });
+  }
+
+}
