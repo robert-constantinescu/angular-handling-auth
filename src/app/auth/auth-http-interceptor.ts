527fc0df50777d17d18cfe1e675f6d1cc1833956
Robert Constantinescu <robertsci@github.com>
2021-03-20 10:53:01 +0200

    using the INTERCEPTOR to handle the RESPONSE that is coming back from the API

--- src/app/auth/auth-http-interceptor.ts
+++ src/app/auth/auth-http-interceptor.ts
@@ -1,19 +1,43 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // here you can basically update any property of the request. Including it's url
-    // by default the HTTP Client will ignore any cookies received unless we add the below `withCredentials` parameter
-    // e.g: Authentication cookies
+    /** here you can basically update any property of the request. Including it's url
+     * by default the HTTP Client will ignore any cookies received unless we add the below `withCredentials` parameter
+     * e.g: Authentication cookies
+     **/
     const reqClone = req.clone({
       withCredentials: true,
     });
-    return next.handle(reqClone);
+
+    /** Behind the scenes, here will be returned an Observable, that is going to emit different events to describe the current status
+     * of the request that we are making
+     * and the RESPONSE that is coming back to us
+     * So we can get some information about this from inside the INTERCEPTOR
+     *
+     * So overall the INTERCEPTOR is not just about intercepting and modifying the outgoing request
+     * we can also use it take a look at the information that is coming back as well and handle it accordingly
+     *
+     * Lesson 304 -  The Modern Angular Bootcamp - Udemy
+     */
+    return next.handle(reqClone)
+      .pipe(
+        tap(val => {
+          if (val.type === HttpEventType.Sent) {
+            console.log('Request was sent to server');
+          }
+
+          if (val.type === HttpEventType.Response) {
+            console.log('Got a response from the API', val);
+          }
+        })
+      );
   }
 
 
