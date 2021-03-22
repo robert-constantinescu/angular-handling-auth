0126acc3d2f4db1f7917057d343de7a81b48e802
Robert Constantinescu <robertsci@github.com>
2021-03-22 19:30:42 +0200

    showing an email list

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+  baseUrl = 'https://api.angular-email.com';
+
+  constructor(private http: HttpClient) { }
+
+  getEmails() {
+    return this.http.get<EmailSummary[]>(`${this.baseUrl}/emails`);
+  }
+
+}
+
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
 }
