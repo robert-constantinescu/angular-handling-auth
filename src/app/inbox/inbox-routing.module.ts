e4987231ec5861672190425bc1b1b561c8bc9c9e
Robert Constantinescu <robertsci@github.com>
2021-03-22 19:45:39 +0200

    create CHILD ROUTE, display different child components based on the ROUTE you are accessing

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {PlaceholderComponent} from './placeholder/placeholder.component';
+import {EmailShowComponent} from './email-show/email-show.component';
 
 
 const routes: Routes = [
@@ -9,7 +11,14 @@ const routes: Routes = [
    * this will load the HomeComponent of the InboxModule
    *
    */
-  {path: '', component: HomeComponent}
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: ':id', component: EmailShowComponent },
+      { path: '', component: PlaceholderComponent }
+    ]
+  }
 ];
 
 @NgModule({
