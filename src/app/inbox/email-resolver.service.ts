967a9c1baff117665aa963678465aa53cdb07b70
Robert Constantinescu <robertsci@github.com>
2021-03-24 18:32:25 +0200

    added some ERROR handling in the resolver

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
 import {Email} from './email';
-import {Observable} from 'rxjs';
+import {EMPTY, Observable} from 'rxjs';
 import {EmailService} from './email.service';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailResolverService  implements Resolve<Email>{
 
-  constructor(private emailService: EmailService) { }
+  constructor(private emailService: EmailService,
+              private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Email> | Promise<Email> | Email {
     // i hope that i will remember that is just some syntactic sugar to get directly the id parameter of the object returned by route.params
@@ -21,7 +23,21 @@ export class EmailResolverService  implements Resolve<Email>{
      *
      * my getEmail() method returns an OBSERVABLE that eventually emits an email
      */
-    return this.emailService.getEmail(id);
+    console.log('the email-resolver');
+    return this.emailService.getEmail(id).pipe(
+      catchError( (err) => {
+        this.router.navigateByUrl('/inbox/not-found');
+        console.log('not-found in Resolver');
+        /**
+         * If anything happens and an error is catched the entire resolver is going to be cancelled
+         * So we don't want to return anything, because the component will be cancelled and will not going to create an instance of it or use it
+         *
+         * This EMPTY is an empty OBSERVABLE that is returned completed in case of error, because catchError require to return an OBSERVABLE
+         * the only reason we have to do this, is basically to make RxJS and TypeScript happy
+         */
+        return EMPTY;
+      })
+    );
   }
 
 
--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -4,6 +4,7 @@ import {HomeComponent} from './home/home.component';
 import {PlaceholderComponent} from './placeholder/placeholder.component';
 import {EmailShowComponent} from './email-show/email-show.component';
 import {EmailResolverService} from './email-resolver.service';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 
 const routes: Routes = [
@@ -15,7 +16,14 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    /**
+     * These children will be loaded in the router-outlet of the HomeComponent.html
+     */
     children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent
+      },
       {
         path: ':id',
         component: EmailShowComponent,
